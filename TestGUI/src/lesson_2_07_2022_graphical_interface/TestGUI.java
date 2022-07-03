96a10c765e08cd8c15b87582132935cdd8e577d3
valentin <delo.biss13@gmail.com>
2022-07-03 16:31:39 +0300

    кодим 3.07.2022

--- TestGUI/src/lesson_2_07_2022_graphical_interface/TestGUI.java
+++ TestGUI/src/lesson_2_07_2022_graphical_interface/TestGUI.java
@@ -1,21 +1,57 @@
 package lesson_2_07_2022_graphical_interface;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.plaf.metal.MetalLookAndFeel;
 import java.awt.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestGUI {
     public static void main(String[] args) {
+
+        try {
+          UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+   //        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+
+
+//        try {
+//            UIManager.setLookAndFeel(new MetalLookAndFeel());
+//        }   catch (UnsupportedLookAndFeelException ex) {
+//            ex.printStackTrace();
+//            Logger.getLogger(TestGUI.class.getName()).log(Level.SEVERE,null,ex);
+//        }
+
+        JFrame.setDefaultLookAndFeelDecorated(true);//true
+                                                    // чтоб использовался скин
+                                                    //UIManager.setLookAndFeel
+                                                    // ("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+
+
         JFrame frame = new JFrame("Test Frame");
+
+        frame.setIconImage(new ImageIcon("D:/Users/13/Pictures/work-logo.jpg").getImage());
         //метод отображения окна (метод отображения окна, по умолчанию -false, true для отображения)
-        frame.setSize(700, 700);
+        frame.setSize(400, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
+        frame.setLocationRelativeTo(null);//запускает фрейм(окно) ровно посередине экрана
         FlowLayout flowLayout = new FlowLayout();
         frame.setLayout(flowLayout);
 
         JPanel panel = new JPanel();
-        panel.setSize(500, 400);
+        panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        panel.setSize(200, 300);
         panel.setBackground(Color.blue);
 
 
@@ -26,6 +62,6 @@ public class TestGUI {
         frame.add(panel);
         panel.add(jButton);
 
-
+        frame.getContentPane().add(panel);
     }
 }
